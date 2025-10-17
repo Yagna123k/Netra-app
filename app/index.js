8b62e8f01e1665a28b43bd17f118905f248e00f1
shanaya-Gupta <shanaya-Gupta@users.noreply.github.com>
2025-10-17 07:14:10 +0000

    fix: Resolve #21 - Create Color Blindness Test Feature

--- app/index.js
+++ app/index.js
@@ -4,3 +4,29 @@ import App from './App';
 import { name as appName } from './app.json';
 
 AppRegistry.registerComponent(appName, () => App);
+
+import 'react-native-gesture-handler';
+import React from 'react';
+import { AppRegistry } from 'react-native';
+
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+
+import App from './App';
+import ColorBlindnessTest from './components/ColorBlindnessTest';
+import { name as appName } from './app.json';
+
+const Stack = createNativeStackNavigator();
+
+function MainApp() {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName="Home">
+        <Stack.Screen name="Home" component={App} />
+        <Stack.Screen name="ColorBlindnessTest" component={ColorBlindnessTest} options={{ title: 'Color Blindness Test' }} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+}
+
+AppRegistry.registerComponent(appName, () => MainApp);
